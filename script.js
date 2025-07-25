6345850676033dd62890e5359a1c9e1287e814ec
SEG0DN1k1 <plevyj0@gmail.com>
2025-07-25 17:12:26 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-﻿// Плавный скролл для якорных ссылок
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
