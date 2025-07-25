4b4a484fb1db7203051318acbf572ad72810e359
SEG0DN1k1 <plevyj0@gmail.com>
2025-07-25 17:11:51 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+﻿// Плавный скролл для якорных ссылок
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
