7459323bb2009866634f9fe76b14055eebdf6b57
larionov <larionov@DT-LARIONOV>
2025-08-15 17:51:52 +0300

    обнова

--- js/script.js
+++ js/script.js
@@ -32,3 +32,30 @@ document.addEventListener('DOMContentLoaded', function () {
         window.addEventListener('scroll', checkScroll);
     }
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Настройки для Intersection Observer
+    const options = {
+        root: null, // Используем viewport как область отслеживания
+        rootMargin: '0px', // Без отступов
+        threshold: 1 // Срабатывает, когда 10% элемента видно
+    };
+
+    // Создаем observer
+    const observer = new IntersectionObserver(function (entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('animated');
+                observer.unobserve(entry.target); // Прекращаем наблюдение после анимации
+            }
+        });
+    }, options);
+
+    // Находим все элементы для анимации
+    const elements = document.querySelectorAll('.animate-on-scroll');
+
+    // Начинаем наблюдение за каждым элементом
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
